670993c8f515c554f76a8a6d0093e54d5c41bc63
Mateusz Nurzyński <mateonurzynski@gmail.com>
2023-08-13 14:24:39 +0200

    Refactor: Lift 'tabs' state from Aside to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,59 @@
+import { useState } from 'react';
+import { useImmer } from 'use-immer';
 import './App.css';
 import Aside from './components/Aside';
 
 function App() {
+  const [tabs, setTabs] = useImmer([
+    {
+      title: 'Basic Information',
+      id: 'basic',
+      fieldSet: [
+        { value: '', label: 'Your full name', id: 'fullName' },
+        { value: '', label: 'E-mail address', id: 'email' },
+        { value: '', label: 'Phone number', id: 'phone' },
+      ],
+    },
+    {
+      title: 'Education',
+      id: 'education',
+      fieldSet: [
+        { value: '', label: 'School name', id: 'schoolName' },
+        { value: '', label: 'Study', id: 'studyName' },
+        { value: '', label: 'Date', id: 'studyDate' },
+      ],
+    },
+    {
+      title: 'Practical experience',
+      id: 'experience',
+      fieldSet: [
+        { value: '', label: 'Company name', id: 'companyName' },
+        { value: '', label: 'Position/Title', id: 'positionTitle' },
+        { value: '', label: 'Responsibilities', id: 'responsibilities' },
+        { value: '', label: 'Start date', id: 'startDate' },
+        { value: '', label: 'End date', id: 'endDate' },
+      ],
+    },
+  ]);
+  const [currentTabId, setCurrentTabId] = useState('basic');
+
+  const currentTabIndex = tabs.findIndex((tab) => {
+    return tab.id === currentTabId;
+  });
+
+  const editField = (newValue, fieldIndex) => {
+    setTabs((tabs) => {
+      tabs[currentTabIndex].fieldSet[fieldIndex].value = newValue;
+    });
+  };
   return (
     <div className='container'>
-      <Aside />
+      <Aside
+        tabs={tabs}
+        onFieldEdit={editField}
+        currentTabId={currentTabId}
+        setCurrentTabId={setCurrentTabId}
+      />
     </div>
   );
 }
--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,56 +1,11 @@
-import { useState } from 'react';
-import { useImmer } from 'use-immer';
 import '../styles/Aside.css';
 import AsideTabButton from './AsideTabButton';
 import Form from './Form';
 
-function Aside() {
-  const [tabs, setTabs] = useImmer([
-    {
-      title: 'Basic Information',
-      id: 'basic',
-      fieldSet: [
-        { value: '', label: 'Your full name', id: 'fullName' },
-        { value: '', label: 'E-mail address', id: 'email' },
-        { value: '', label: 'Phone number', id: 'phone' },
-      ],
-    },
-    {
-      title: 'Education',
-      id: 'education',
-      fieldSet: [
-        { value: '', label: 'School name', id: 'schoolName' },
-        { value: '', label: 'Study', id: 'studyName' },
-        { value: '', label: 'Date', id: 'studyDate' },
-      ],
-    },
-    {
-      title: 'Practical experience',
-      id: 'experience',
-      fieldSet: [
-        { value: '', label: 'Company name', id: 'companyName' },
-        { value: '', label: 'Position/Title', id: 'positionTitle' },
-        { value: '', label: 'Responsibilities', id: 'responsibilities' },
-        { value: '', label: 'Start date', id: 'startDate' },
-        { value: '', label: 'End date', id: 'endDate' },
-      ],
-    },
-  ]);
-  const [currentTabId, setCurrentTabId] = useState('basic');
-
+function Aside({ tabs, onFieldEdit, currentTabId, setCurrentTabId }) {
   const currentTab = tabs.find((tab) => {
     return tab.id === currentTabId;
   });
-
-  const currentTabIndex = tabs.findIndex((tab) => {
-    return tab.id === currentTabId;
-  });
-
-  const editField = (newValue, fieldIndex) => {
-    setTabs((tabs) => {
-      tabs[currentTabIndex].fieldSet[fieldIndex].value = newValue;
-    });
-  };
 
   return (
     <div className='aside'>
@@ -71,7 +26,7 @@ function Aside() {
       <Form
         tab={currentTab}
         onChange={(e) => {
-          editField(e.target.value, e.target.getAttribute('data-index'));
+          onFieldEdit(e.target.value, e.target.getAttribute('data-index'));
         }}
       />
     </div>
